659c4554ebe87166c357ed0f9ea8e3f835ea5393
Quentin Young <qlyoung@cumulusnetworks.com>
2018-04-02 12:38:03 -0400

    cleanup config.fish
    Signed-off-by: Quentin Young <qlyoung@cumulusnetworks.com>

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,11 +1,17 @@
+# no greeting
 set fish_greeting ""
-set EDITOR "vim"
 set TERM xterm-256color
 set color_cwd
-set HOMEBREW_NO_ANALYTICS 1
 set -x fish_user_paths "/usr/local/bin" $fish_user_paths
 set -x fish_user_paths "/usr/local/sbin" $fish_user_paths
-set -x EDITOR nvim
+
+# required by gpg-agent
 set -x GPG_TTY (tty)
+
+# Homebrew
+set HOMEBREW_NO_ANALYTICS 1
+
+# vim
+set -x EDITOR nvim
 alias vim nvim
 alias vimdiff 'nvim -d'
