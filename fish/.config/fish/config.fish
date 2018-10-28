3f1f935abe59292592aa020f20fcd1561c108bed
Quentin Young <qlyoung@cumulusnetworks.com>
2018-10-28 01:58:21 +0000

    fish: add git branch indicator to prompt
    7,000,000 invocations of `git status` later, I give up. The hipsters
    were right.

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -15,3 +15,28 @@ set HOMEBREW_NO_ANALYTICS 1
 set -x EDITOR nvim
 alias vim nvim
 alias vimdiff 'nvim -d'
+alias rf 'sudo systemctl restart frr'
+
+# Shell prompt
+set fish_git_dirty_color red
+set fish_git_not_dirty_color green
+
+function parse_git_branch
+  set -l branch (git branch 2> /dev/null | grep -e '\* ' | sed 's/^..\(.*\)/\1/')
+  set -l git_status (git status -s)
+
+  if test -n "$git_status"
+    echo (set_color $fish_git_dirty_color)$branch(set_color normal)
+  else
+    echo (set_color $fish_git_not_dirty_color)$branch(set_color normal)
+  end
+end
+
+function fish_prompt
+  set -l git_dir (git rev-parse --git-dir 2> /dev/null)
+  if test -n "$git_dir"
+    printf '%s@%s [%s] %s%s%s> ' (whoami) (hostname|cut -d . -f 1) (parse_git_branch) (set_color $fish_color_cwd) (prompt_pwd) (set_color normal)
+  else
+    printf '%s@%s %s%s%s> ' (whoami) (hostname|cut -d . -f 1) (set_color $fish_color_cwd) (prompt_pwd) (set_color normal)
+  end
+end
