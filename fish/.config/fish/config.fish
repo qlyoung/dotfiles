3d378768727c6015ddafb8f118a1bb6c92982d26
Quentin Young <qlyoung@qlyoung.net>
2022-11-21 22:13:00 -0500

    fish: misc updates

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -9,10 +9,20 @@ set -x GOPATH /home/qlyoung/go
 set -x fish_user_paths "$GOPATH/bin" $fish_user_paths
 set -x fish_user_paths "/home/qlyoung/.cargo/bin" $fish_user_paths
 set -x fish_user_paths "$HOME/.poetry/bin" $fish_user_paths
+set -x fish_user_paths "$HOME/scripts/nvidia" $fish_user_paths
+set -x fish_user_paths "$HOME/scripts/nvidia/ngc" $fish_user_paths
+set -x fish_user_paths "$HOME/scripts/misc" $fish_user_paths
+set -x NDAS_PDS /home/qlyoung/projects/ndas/private/doc/playbooks/service/
+set -x NDAS_SMITH /home/qlyoung/projects/ndas/tools/smith/
+
+set -x SSH_AUTH_SOCK /run/user/1000/ssh-agent.socket
 
 # set -x fish_user_paths (dirname (go list -f '{{.Target}}' .)) $fish_user_paths
 
 # alias clear "math (random) \% 10 == 0; and timeout 2 cbeams -o; clear; or clear"
+
+# Poetry
+export PATH="/home/qlyoung/.local/bin:$PATH"
 
 # X dpi scaling
 #set -g -x GDK_SCALE 2
@@ -33,27 +43,35 @@ alias vimdiff 'nvim -d'
 # Shell prompt
 set fish_git_dirty_color red
 set fish_git_not_dirty_color green
+set fish_color_user blue
+set fish_color_cwd ff781f
+
+function print_fish_colors --description 'Shows the various fish colors being used'
+    set -l clr_list (set -n | grep fish | grep color | grep -v __)
+    if test -n "$clr_list"
+        set -l bclr (set_color normal)
+        set -l bold (set_color --bold)
+        printf "\n| %-38s | %-38s |\n" Variable Definition
+        echo '|¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯|¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯|'
+        for var in $clr_list
+            set -l def $$var
+            set -l clr (set_color $def)
+            or begin
+                printf "| %-38s | %s%-38s$bclr |\n" "$var" (set_color --bold white --background=red) "$def"
+                continue
+            end
+            printf "| $clr%-38s$bclr | $bold%-38s$bclr |\n" "$var" "$def"
+        end
+        echo '|________________________________________|________________________________________|'\n
+    end
+end
+
 
 function cb
   echo (git status -sb | head -n 1 | cut -d' ' -f2)
 end
 
-function parse_git_branch
-  set -l branch (git branch 2> /dev/null | grep -e '\* ' | sed 's/^..\(.*\)/\1/')
-  set -l git_status (git status -s)
-
-  if test -n "$git_status"
-    echo (set_color $fish_git_dirty_color)$branch(set_color normal)
-  else
-    echo (set_color $fish_git_not_dirty_color)$branch(set_color normal)
-  end
-end
-
-function fish_prompt_old
-  set -l git_dir (git rev-parse --git-dir 2> /dev/null)
-  if test -n "$git_dir"
-    printf '%s@%s [%s] %s%s%s> ' (whoami) (hostname|cut -d . -f 1) (parse_git_branch) (set_color $fish_color_cwd) (prompt_pwd) (set_color normal)
-  else
-    printf '%s@%s %s%s%s> ' (whoami) (hostname|cut -d . -f 1) (set_color $fish_color_cwd) (prompt_pwd) (set_color normal)
-  end
+function tere
+    set --local result (/usr/bin/tere $argv)
+    [ -n "$result" ] && cd -- "$result"
 end
